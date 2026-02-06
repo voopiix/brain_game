b74522fd8e8308fd5f6f1c3a50e78bd4091add92
voopiix <Kkarklina1@anip.lv>
2026-02-06 10:19:51 +0200

    11

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import json
 
 base_dir = Path(__file__).resolve().parent
-app = Flask(__name__, tempate_folder=str(base_dir / 'templates'), static_folder=str(base_dir / 'static'))
+app = Flask(__name__, template_folder=str(base_dir / 'templates'), static_folder=str(base_dir / 'static'))
 
 @app.route('/')
 def index():
